a4de1a8bd389655871370dad9ed0810415b1159b
maximilionus <xxxarakxxx@gmail.com>
2019-11-05 19:58:03 +0300

    Base structure

---
+++ a2sfa3_jsrs_fix/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches
+{
+	class MyAddon
+	{
+		name = "Arma 2 Sounds For Arma 3 - JSRS Compatibility Fix";
+		author = "MAXIMILI";
+		requiredVersion = 0.1; 
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+	};
+};
