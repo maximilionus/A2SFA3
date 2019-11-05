c00e94aae2e67c66cf80e726fef20bd44dff2865
maximilionus <xxxarakxxx@gmail.com>
2019-11-05 21:21:44 +0300

    added jsrs to req list

--- a2sfa3_jsrs_fix/config.cpp
+++ a2sfa3_jsrs_fix/config.cpp
@@ -1,11 +1,18 @@
 class CfgPatches
 {
-	class MyAddon
+	class a2sfa3_jsrs_compatibility_fix
 	{
 		name = "Arma 2 Sounds For Arma 3 - JSRS Compatibility Fix";
 		author = "MAXIMILI";
 		requiredVersion = 0.1; 
-		requiredAddons[] = {};
+		requiredAddons[] =
+		{
+			"jsrs_soundmod_complete_edition_soundfiles",
+			"jsrs_soundmod_complete_edition",
+			"jsrs_soundmod_movement",
+			"jsrs_soundmod_environment",
+			"a2sfa3_sounds"
+		};
 		units[] = {};
 		weapons[] = {};
 	};
